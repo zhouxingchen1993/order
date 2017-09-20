17716a4cdd10980364f0e4cc800849bb817731c6
zhouxingchen1993 <zhouxingchen93@163.com>
2017-09-20 22:40:57 +0800

    order

---
+++ src/main/java/com/htsat/order/service/IOrderService.java
@@ -0,0 +1,34 @@
+package com.htsat.order.service;
+
+import com.htsat.order.dto.OrderDTO;
+import com.htsat.order.dto.OrderSKUDTO;
+import com.htsat.order.model.REcSku;
+import java.util.List;
+
+public interface IOrderService {
+
+    /**
+     * create order
+     */
+    OrderDTO createOrderAndDeliveryAndOrderSKU(OrderDTO orderDTO) throws Exception;
+
+    void createOrderAndDeliveryAndOrderSKUToRedis(OrderDTO orderDTO) throws Exception;
+
+    /**
+     * get order
+     */
+    OrderDTO getOrderAndDeliveryAndOrderSKUAndAddress(String orderId) throws Exception;
+
+    /**
+     * delete order
+     */
+    void deleteOrderAndDeliveryAndOrderSKU(String orderId) throws Exception;
+
+    /**
+     *  check method
+     */
+    boolean checkSKUParam(List<OrderSKUDTO> orderskudtoList, List<REcSku> skuList);
+
+    List<REcSku> getSKUListByDTOList(List<OrderSKUDTO> orderskudtoList);
+
+}
